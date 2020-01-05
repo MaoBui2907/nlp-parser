c9cb643f23746143c63fe0d19a8cf02ea4b740c6
nguyenmao2101 <maonguyen199873@gmail.com>
2020-01-05 22:50:19 +0700

    5 câu (danh từ + giới từ) + động ngữ -- lỗi j đó

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ import nltk
 grammar = nltk.grammar.FeatureGrammar.fromstring("""
 S -> NP[rn=?x] VP[rn=?x]
 
-NP[rn=?x, jj=?y] -> NN[rn=?x, jj=?y]
-NP[rn=?x, jj=?y] -> NNP[rn=?x, jj=?y]
-NP[rn=?x, jj=?y] -> PRP[rn=?x, jj=?y]
-NP[rn=?x, jj=?y] -> UNN[rn=?x] NN[rn=?x, jj=?y]
+NP[rn=?x, jj=?y, in=?z] -> NN[rn=?x, jj=?y, in=?z]
+NP[rn=?x, jj=?y, in=?z] -> NNP[rn=?x, jj=?y, in=?z]
+NP[rn=?x, jj=?y, in=?z] -> PRP[rn=?x, jj=?y, in=?z]
+NP[rn=?x, jj=?y, in=?z] -> UNN[rn=?x] NN[rn=?x, jj=?y, in=?z]
+NP[rn=?x, jj=?y, in=?z] -> NN[rn=?x, jj=?y, in=?z] NN[rn=?x, jj=?y, in=?z]
 
-NP[rn=?x, jj=?y] -> NP1[rn=?x, jj=?y] JJ[rn=?x, jj=?y]
-NP1[rn=?x, jj=?y] -> NN[rn=?x, jj=?y]
-NP1[rn=?x, jj=?y] -> NNP[rn=?x, jj=?y]
-NP1[rn=?x, jj=?y] -> PRP[rn=?x, jj=?y]
-NP1[rn=?x, jj=?y] -> UNN[rn=?x] NN[rn=?x, jj=?y] 
+NP[rn=?x, jj=?y, in=?z] -> NP1[rn=?x] PP[]
+NP[rn=?x, jj=?y, in=?z] -> NP1[rn=?x, jj=?y, in=?z] JJ[rn=?x, jj=?y, in=?z]
+NP1[rn=?x, jj=?y, in=?z] -> NN[rn=?x, jj=?y, in=?z]
+NP1[rn=?x, jj=?y, in=?z] -> NNP[rn=?x, jj=?y, in=?z]
+NP1[rn=?x, jj=?y, in=?z] -> PRP[rn=?x, jj=?y, in=?z]
+NP1[rn=?x, jj=?y, in=?z] -> UNN[rn=?x] NN[rn=?x, jj=?y, in=?z] 
+
 
 VP[rn=?x] -> VB[rn=?x]
 VP[rn=?x] -> VB[rn=?x, rd=?y] NP[rn=?y]
+VP[rn=?x] -> VP[rn=?x] PP[]
+
+PP[] -> IN[in=?z] NP[in=?z]
 
 PRP[rn=nguoi] -> "tôi"
 
@@ -24,19 +30,32 @@ NNP[rn=nguoi] -> "Nam"
 
 UNN[rn=dong_vat] -> "con"
 UNN[rn=dong_vat] -> "đàn"
+UNN[rn=do_vat] -> "chiếc"
 
 NN[rn=dong_vat] -> "cá"
 NN[rn=dong_vat] -> "chim"
 NN[rn=vat_the] -> "mặt_trời"
-NN[rn=thuc_vat] -> "cỏ"
 NN[rn=dong_vat] -> "voi"
+NN[rn=thuc_vat] -> "cỏ"
+NN[rn=thuc_vat] -> "cây"
 NN[rn=do_vat] -> "xe"
 NN[rn=do_vat] -> "xương"
+NN[rn=su_vat] -> "bãi"
+NN[rn=su_vat, in=ao] -> "ao"
+NN[rn=su_vat, in=bo] -> "bờ"
+NN[rn=su_vat] -> "căn"
+NN[rn=su_vat] -> "nhà"
+NN[rn=do_vat, jj=vang] -> "lá"
 NN[rn=dong_vat, jj=den] -> "mèo"
 NN[rn=dong_vat, jj=gay] -> "ngựa"
 NN[rn=dong_vat, jj=nho] -> "kiến"
 NN[rn=dong_vat, jj=nho] -> "chó"
 NN[rn=dong_vat, jj=vang] -> "nai"
+
+IN[] -> "của"
+IN[] -> "trên"
+IN[in=ao] -> "trong"
+IN[in=bo] -> "lên"
 
 JJ[jj=den] -> "đen"
 JJ[jj=gay] -> "gầy"
@@ -48,6 +67,9 @@ VB[rn=nguoi] -> "chơi"
 VB[rn=dong_vat] -> "bơi"
 VB[rn=dong_vat] -> "bay"
 VB[rn=vat_the] -> "mọc"
+VB[rn=dong_vat] -> "nhảy"
+VB[rn=su_vat] -> "rụng"
+VB[rn=su_vat] -> "cháy"
 VB[rn=dong_vat, rd=dong_vat] -> "tấn_công"
 VB[rn=dong_vat, rd=dong_vat] -> "ăn"
 VB[rn=dong_vat, rd=do_vat] -> "kéo"
@@ -69,10 +91,17 @@ sentences_2 = ["đàn kiến nhỏ tấn_công con voi",
                "con chó nhỏ gặm xương",
                "con nai vàng ăn cỏ"]
 
-tests = ["con chó nhỏ ăn cỏ"]
+sentences_3 = ["con chó của tôi chơi trên bãi cỏ",
+               "con cá trong ao nhảy lên bờ",
+               "chiếc lá vàng trên cây rụng",
+               "căn nhà của Nam cháy",
+               "con mèo của tôi ngủ"]
 
-for sent in sentences_1:
+tests = ["con chó của tôi chơi trên bãi cỏ"]
+
+for sent in sentences_3:
     print(sent)
     words = sent.split()
     for tree in parser.parse(words):
+        print(tree)
         tree.draw()
