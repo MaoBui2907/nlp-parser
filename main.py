4f0482679aaeb1361af687ab817d51860fb8b11c
nguyenmao2101 <maonguyen199873@gmail.com>
2020-01-06 21:37:44 +0700

    fix

--- main.py
+++ main.py
@@ -8,10 +8,17 @@ NP[rn=?x, jj=?y, in=?z] -> NN[rn=?x, jj=?y, in=?z]
 NP[rn=?x, jj=?y, in=?z] -> NNP[rn=?x, jj=?y, in=?z]
 NP[rn=?x, jj=?y, in=?z] -> PRP[rn=?x, jj=?y, in=?z]
 NP[rn=?x, jj=?y, in=?z] -> UNN[rn=?x] NN[rn=?x, jj=?y, in=?z]
-NP[rn=?x, jj=?y, in=?z] -> NN[rn=?x, jj=?y, in=?z] NN[rn=?x, jj=?y, in=?z]
+NP[rn=?x, jj=?y, in=?z] -> NP[rn=?x, jj=?y, in=?z] JJ[jj=?y]
 
-NP[rn=?x, jj=?y, in=?z] -> NP1[rn=?x] PP[]
-NP[rn=?x, jj=?y, in=?z] -> NP1[rn=?x, jj=?y, in=?z] JJ[rn=?x, jj=?y, in=?z]
+NP[rn=?x, jj=?y, in=?z] -> NP2[rn=?x, jj=?y, in=?z] JJ[jj=?y]
+NP[rn=?x, jj=?y, in=?z] -> NP2[rn=?x] PP[]
+NP[rn=?x, jj=?y, in=?z] -> NP2[rn=?x, jj=?y, in=?z] JJ[jj=?y]
+NP2[rn=?x, jj=?y, in=?z] -> NP1[rn=?x] PP[]
+NP2[rn=?x, jj=?y, in=?z] -> NP1[rn=?x, jj=?y, in=?z] JJ[jj=?y]
+NP2[rn=?x, jj=?y, in=?z] -> NN[rn=?x, jj=?y, in=?z]
+NP2[rn=?x, jj=?y, in=?z] -> NNP[rn=?x, jj=?y, in=?z]
+NP2[rn=?x, jj=?y, in=?z] -> PRP[rn=?x, jj=?y, in=?z]
+NP2[rn=?x, jj=?y, in=?z] -> UNN[rn=?x] NN[rn=?x, jj=?y, in=?z] 
 NP1[rn=?x, jj=?y, in=?z] -> NN[rn=?x, jj=?y, in=?z]
 NP1[rn=?x, jj=?y, in=?z] -> NNP[rn=?x, jj=?y, in=?z]
 NP1[rn=?x, jj=?y, in=?z] -> PRP[rn=?x, jj=?y, in=?z]
@@ -31,6 +38,8 @@ NNP[rn=nguoi] -> "Nam"
 UNN[rn=dong_vat] -> "con"
 UNN[rn=dong_vat] -> "đàn"
 UNN[rn=do_vat] -> "chiếc"
+UNN[rn=thuc_vat] -> "bãi"
+UNN[rn=do_vat] -> "căn"
 
 NN[rn=dong_vat] -> "cá"
 NN[rn=dong_vat] -> "chim"
@@ -40,11 +49,9 @@ NN[rn=thuc_vat] -> "cỏ"
 NN[rn=thuc_vat] -> "cây"
 NN[rn=do_vat] -> "xe"
 NN[rn=do_vat] -> "xương"
-NN[rn=su_vat] -> "bãi"
 NN[rn=su_vat, in=ao] -> "ao"
 NN[rn=su_vat, in=bo] -> "bờ"
-NN[rn=su_vat] -> "căn"
-NN[rn=su_vat] -> "nhà"
+NN[rn=do_vat] -> "nhà"
 NN[rn=do_vat, jj=vang] -> "lá"
 NN[rn=dong_vat, jj=den] -> "mèo"
 NN[rn=dong_vat, jj=gay] -> "ngựa"
@@ -63,13 +70,15 @@ JJ[jj=nho] -> "nhỏ"
 JJ[jj=vang] -> "vàng"
 
 VB[rn=nguoi] -> "ngủ"
+VB[rn=dong_vat] -> "ngủ"
 VB[rn=nguoi] -> "chơi"
+VB[rn=dong_vat] -> "chơi"
 VB[rn=dong_vat] -> "bơi"
 VB[rn=dong_vat] -> "bay"
 VB[rn=vat_the] -> "mọc"
 VB[rn=dong_vat] -> "nhảy"
-VB[rn=su_vat] -> "rụng"
-VB[rn=su_vat] -> "cháy"
+VB[rn=do_vat] -> "rụng"
+VB[rn=do_vat] -> "cháy"
 VB[rn=dong_vat, rd=dong_vat] -> "tấn_công"
 VB[rn=dong_vat, rd=dong_vat] -> "ăn"
 VB[rn=dong_vat, rd=do_vat] -> "kéo"
@@ -97,11 +106,10 @@ sentences_3 = ["con chó của tôi chơi trên bãi cỏ",
                "căn nhà của Nam cháy",
                "con mèo của tôi ngủ"]
 
-tests = ["con chó của tôi chơi trên bãi cỏ"]
-
+tests = ["chiếc lá vàng trên cây rụng"]
+tests2 = ["con cá trong ao nhảy lên bờ"]
 for sent in sentences_3:
     print(sent)
     words = sent.split()
     for tree in parser.parse(words):
-        print(tree)
         tree.draw()
